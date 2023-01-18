e74adb43504e399bae2175b896dfa92a00f388ef
SomisettyAlekhya <96366568+SomisettyAlekhya@users.noreply.github.com>
2023-01-18 10:08:13 +0530

    Update talentbattleproject (1).py

--- talentbattleproject (1).py
+++ talentbattleproject (1).py
@@ -168,16 +168,16 @@ stopword=set (stopwords . words ( 'english' ))
 
 
 def clean(text):
-    text = str(text) . lower()  #returns a string where all characters are lower case. Symbols and Numbers are ignored.
-    text = re. sub('\[.*?\]',' ',text)  #substring and returns a string with replaced values.
-    text = re. sub('https?://\S+/www\. \S+', ' ', text)#whitespace char with pattern
-    text = re. sub('<. *?>+', ' ', text)#special char enclosed in square brackets
-    text = re. sub(' [%s]' % re. escape(string. punctuation), ' ', text)#eliminate punctuation from string
+    text = str(text) . lower()  
+    text = re. sub('\[.*?\]',' ',text) 
+    text = re. sub('https?://\S+/www\. \S+', ' ', text)
+    text = re. sub('<. *?>+', ' ', text)
+    text = re. sub(' [%s]' % re. escape(string. punctuation), ' ', text)
     text = re. sub(' \n',' ', text)
-    text = re. sub(' \w*\d\w*' ,' ', text)#word character ASCII punctuation
-    text = [word for word in text. split(' ') if word not in stopword]  #removing stopwords
+    text = re. sub(' \w*\d\w*' ,' ', text)
+    text = [word for word in text. split(' ') if word not in stopword]  
     text =" ". join(text)
-    text = [stemmer . stem(word) for word in text. split(' ') ]#remove morphological affixes from words
+    text = [stemmer . stem(word) for word in text. split(' ') ]
     text = " ". join(text)
     return text
 df [ "text"] = df["text"]. apply(clean)
